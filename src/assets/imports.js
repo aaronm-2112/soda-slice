71aea99dcb075369fe0914e73903772c2a257c33
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-01-14 16:03:01 -0800

    Importing html and scripts.

--- src/assets/imports.js
+++ src/assets/imports.js
@@ -4,6 +4,8 @@ const links = document.querySelectorAll('link[rel="import"]');
 Array.prototype.forEach.call(links, (link) => {
   let template = link.import.querySelector(".task-template");
   let clone = document.importNode(template.content, true);
+
+  console.log(clone)
   //if (link.href.match('about.html')) {
   //  document.querySelector('body').appendChild(clone)
   //} else {
